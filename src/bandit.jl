26195c1ae7e95f72c90db1fb604ebb2672e3e6ac
Sydney Katz <sydney.m.katz@wustl.edu>
2022-10-25 15:44:43 -0700

    initial bandit algorithm implementation

---
+++ src/bandit.jl
@@ -0,0 +1,51 @@
+# Bandit model
+using GridInterpolations
+using Distributions
+
+mutable struct BanditModel
+    grid::RectangleGrid # Grid to evaluate on
+    pfail::Vector # Estimated probability of failure at each point in the grid
+    α::Vector # Failure counts
+    β::Vector # Success counts
+end
+
+function run_estimation!(model::BanditModel, problem::GriddedProblem, acquisition, nsamps;
+                         log_every=Inf)
+    set_sizes = [0]
+    
+    for i in ProgressBar(1:nsamps)
+        sample_ind = acquisition(model)
+        params = ind2x(model.grid, sample_ind)
+        res = problem.sim(params, model.nsamps)
+        nfail = sum(res)
+
+
+        model.α[sample_ind] += nfail
+        model.β[sample_ind] += 1 - nfail
+        model.pfail[sample_ind] = model.α[sample_ind] / model.β[sample_ind]
+
+        if (i % log_every) == 0
+            estimate_from_counts!(problem, model)
+            push!(set_sizes, sum(problem.is_safe))
+        end
+    end
+
+    return set_sizes
+end
+
+function estimate_from_pfail!(problem::GriddedProblem, model::BanditModel)
+    for i = 1:length(problem.grid)
+        params = ind2x(problem.grid, i)
+        pfail = interpolate(model.grid, model.pfail, params)
+        problem.is_safe[i] = pfail < problem.pfail_threshold
+    end
+end
+
+function estimate_from_counts!(problem::GriddedProblem, model::BanditModel)
+    for i = 1:length(problem.grid)
+        params = ind2x(problem.grid, i)
+        α = interpolate(model.grid, model.α, params)
+        β = interpolate(model.grid, model.β, params)
+        problem.is_safe[i] = cdf(Beta(α, β), problem.pfail_threshold) > problem.conf_threshold
+    end
+end
