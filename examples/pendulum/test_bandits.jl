2fc774920efc5614b5772dbef5205fce2744200f
Sydney Katz <sydney.m.katz@wustl.edu>
2022-11-21 15:24:12 -0800

    some code for visualizing bandit solution

--- examples/pendulum/test_bandits.jl
+++ examples/pendulum/test_bandits.jl
@@ -45,13 +45,13 @@ function to_heatmap(grid::RectangleGrid, vals; kwargs...)
 end
 
 # Ground truth
-# model_gt = BSON.load("examples/pendulum/results/ground_truth.bson")[:model]
-# problem_gt = pendulum_problem(100, 100, σθ_max=0.2, σω_max=1.0, conf_threshold=0.95)
-# estimate_from_pfail!(problem_gt, model_gt)
+model_gt = BSON.load("examples/pendulum/results/ground_truth.bson")[:model]
+problem_gt = pendulum_problem(100, 100, σθ_max=0.2, σω_max=1.0, conf_threshold=0.95)
+estimate_from_pfail!(problem_gt, model_gt)
 
 # Set up the problem
-nθ = 101
-nω = 101
+nθ = 100
+nω = 100
 σθ_max = 0.2
 σω_max = 1.0
 problem = pendulum_problem(nθ, nω, σθ_max=σθ_max, σω_max=σω_max, conf_threshold=0.95)
@@ -66,34 +66,69 @@ p = plot(collect(0:nsamps), set_sizes_random, label="random", legend=:topleft, l
 # Gittens Allocation Index Acquistion
 nsamps = 50000
 model_gi = pendulum_bandit_model(nθ, nω, σθ_max=σθ_max, σω_max=σω_max)
-gi = BSON.load("src/gittens_data/gi_1000pulls_beta99.bson")[:gi]
-gi_acquisition(model) = gittens_acquisition(model, problem.pfail_threshold, problem.conf_threshold, gi)
+gi = BSON.load("src/gittens_data/gi_1000pulls_beta9999.bson")[:gi]
+gi_acquisition(model) = gittens_acquisition(model, problem.pfail_threshold, problem.conf_threshold, gi, 
+                                            rand_argmax=false)
 set_sizes_gi = run_estimation!(model_gi, problem, gi_acquisition, nsamps)
-
-model_gie = pendulum_bandit_model(nθ, nω, σθ_max=σθ_max, σω_max=σω_max)
-gie = BSON.load("src/gittens_data/gi_1000pulls_beta9999.bson")[:gi]
-gi_acquisition(model) = gittens_acquisition(model, problem.pfail_threshold, problem.conf_threshold, gie)
-set_sizes_gie = run_estimation!(model_gie, problem, gi_acquisition, nsamps)
-
-model_gier = pendulum_bandit_model(nθ, nω, σθ_max=σθ_max, σω_max=σω_max)
-gie = BSON.load("src/gittens_data/gi_1000pulls_beta9999.bson")[:gi]
-gi_acquisition(model) = gittens_acquisition(model, problem.pfail_threshold, problem.conf_threshold, gie, rand_argmax=true)
-set_sizes_gie = run_estimation!(model_gie, problem, gi_acquisition, nsamps)
 
 plot!(p, collect(0:nsamps), set_sizes_gi, label="Gittens 0.99", legend=:topleft, linetype=:steppre,
     xlabel="Number of Episodes", ylabel="Safe Set Size")
 
-plot!(p, collect(0:nsamps), set_sizes_gie, label="Gittens 0.9999", legend=:topleft, linetype=:steppre,
-    xlabel="Number of Episodes", ylabel="Safe Set Size")
-
 plot_eval_points(model_random)
 plot_eval_points(model_gi)
-plot_eval_points(model_gie)
 
 to_heatmap(model_random.grid, model_random.α .+ model_random.β, c=:thermal)
 to_heatmap(model_gi.grid, model_gi.α .+ model_gi.β, c=:thermal)
-to_heatmap(model_gi.grid, model_gie.α .+ model_gie.β, c=:thermal)
 
+function plot_summary_gt(model::BanditModel, problem::GriddedProblem, iter)
+    eval_inds = model.eval_inds[1:iter]
+    neval = [length(findall(eval_inds .== i)) for i = 1:length(model.grid)]
+    p1 = to_heatmap(model.grid, neval, c=:thermal, clims=(0, 125),
+        xlabel="σθ", ylabel="σω", title="Number of Episodes")
+
+    # is_safe = falses(length(model.grid))
+    # for i = 1:length(model.grid)
+    #     params = ind2x(model.grid, i)
+    #     α, β = predict_beta(model, params)
+    #     is_safe[i] = cdf(Beta(α, β), problem.pfail_threshold) > problem.conf_threshold
+    # end
+
+    # eval_set = unique(eval_inds)
+    # xs_eval = [ind2x(model.grid, i)[1] for i in eval_set]
+    # ys_eval = [ind2x(model.grid, i)[2] for i in eval_set]
+
+    # colors = zeros(length(is_safe))
+    # FN_inds = findall(.!is_safe .& problem.is_safe)
+    # !isnothing(FN_inds) ? colors[FN_inds] .= 0.25 : nothing
+    # TN_inds = findall(.!is_safe .& .!problem.is_safe)
+    # !isnothing(TN_inds) ? colors[TN_inds] .= 0.5 : nothing
+    # FP_inds = finalize(is_safe .& .!problem.is_safe)
+    # !isnothing(FP_inds) ? colors[FP_inds] .= 0.75 : nothing
+
+    # if sum(is_safe) > 0
+    #     p2 = to_heatmap(model.grid, colors,
+    #         c=cgrad(mycmap, 4, categorical=true), colorbar=:none,
+    #         title="Estimated Safe Set", xlabel="σθ", ylabel="σω")
+    # else
+    #     p2 = to_heatmap(model.grid, colors,
+    #         c=cgrad(mycmap_small, 2, categorical=true), colorbar=:none,
+    #         title="Estimated Safe Set", xlabel="σθ", ylabel="σω")
+    # end
+    # scatter!(p2, xs_eval, ys_eval,
+    #     markersize=1.0, markercolor=:green, markerstrokecolor=:green,
+    #     xlabel="σθ", ylabel="σω", legend=false)
+
+    return p1 #plot(p1, p2, size=(800, 400))
+end
+
+plot_summary_gt(model_gi, problem_gt, 100)
+
+anim = @animate for iter in 1:500:50000
+    plot_summary_gt(model_gi, problem_gt, iter)
+end
+Plots.gif(anim, "figs/gi_norand_example.gif", fps=10)
+
+# Plotting Gittens Index
 function get_heat(x, y)
     xi = convert(Int64, round(x))
     yi = convert(Int64, round(y))
@@ -108,44 +143,3 @@ end
 
 heatmap(0:999, 0:999, get_heat, xlabel="α", ylabel="β")
 plot(1:999, (x) -> gi(x, 1), legend=false, xlabel="α", ylabel="gi(α, 1)")
-
-plot(collect(0:49999), model_gi.eval_inds)
-eval_xs = [ind2x(model_gi.grid, model_gi.eval_inds[i])[1] for i = 1:length(model_gi.eval_inds)]
-plot(collect(0:49999), eval_xs)
-
-nothing
-# # LCB acquisition
-# nsamps = 50000
-# model_lcb = pendulum_bandit_model(nθ, nω, σθ_max=σθ_max, σω_max=σω_max)
-# lcb_acquisition(model) = lcb_acquisition(model, problem.pfail_threshold, problem.conf_threshold, c=0.1)
-# set_sizes_lcb = run_estimation!(model_lcb, problem, lcb_acquisition, nsamps)
-
-# plot!(p, collect(0:nsamps), set_sizes_lcb, label="LCB", legend=:topleft, linetype=:steppre,
-#     xlabel="Number of Episodes", ylabel="Safe Set Size")
-
-# function to_heatmap(grid::RectangleGrid, vals; kwargs...)
-#     vals_mat = reshape(vals, length(grid.cutPoints[1]), length(grid.cutPoints[2]))
-#     return heatmap(grid.cutPoints[1], grid.cutPoints[2], vals_mat'; kwargs...)
-# end
-
-# to_heatmap(model_random.grid, model_random.α .+ model_random.β, c=:thermal)
-# to_heatmap(model_lcb.grid, model_lcb.α .+ model_random.β, c=:thermal)
-
-# function plot_eval_points(model::BanditModel)
-#     xs = [pt[1] for pt in model.grid]
-#     ys = [pt[2] for pt in model.grid]
-#     p = scatter(xs, ys, legend=false,
-#         markersize=0.5, markercolor=:black, markerstrokecolor=:black)
-
-#     xs_eval = [ind2x(model.grid, i)[1] for i in unique(model.eval_inds)]
-#     ys_eval = [ind2x(model.grid, i)[2] for i in unique(model.eval_inds)]
-#     scatter!(p, xs_eval, ys_eval,
-#         markersize=2.0, markercolor=:green, markerstrokecolor=:green,
-#         xlabel="σθ", ylabel="σω")
-#     return p
-# end
-
-# plot_eval_points(model_random)
-# plot_eval_points(model_lcb)
-
-# plot(model_lcb.eval_inds)
